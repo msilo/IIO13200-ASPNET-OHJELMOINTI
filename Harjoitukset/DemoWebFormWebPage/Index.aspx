4ba5de04170bf84a7fa889e1299841d932fcb8f4
MS <g8499@student.jamk.fi>
2016-10-20 15:04:06 +0700

    Started Exercise 1. Viewing txt-file with Razor 3 and Web Form

---
+++ Harjoitukset/DemoWebFormWebPage/Index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <%foreach (string item in Lines)
+            {%>
+                <%= item %>
+            <%}
+        %>
+    </div>
+        <%= Lines[0] %>
+    </form>
+</body>
+</html>
---
+++ Harjoitukset/DemoWebFormWebPage/Index.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Index : System.Web.UI.Page
+{
+    private string[] lines;
+
+    public string[] Lines
+    {
+        get { return lines; }
+        set { lines = value; }
+    }
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            lines = new string[10];
+            lines = ReadFile(Server.MapPath("~/App_Data/Oppilaat.txt"));
+        }
+        catch (Exception ex)
+        {
+
+            Response.Write(ex.Message);
+        }
+    }
+
+    private string[] ReadFile(string path)
+    {
+        string[] lines = File.ReadAllLines(path);
+        return lines;
+    }
+}
